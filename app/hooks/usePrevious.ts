1ebb2a46d165bf74794baeadfdba5d468941cad7
gabrielslach <gabdl.dev@gmail.com>
2022-06-08 00:42:34 +0800

    :wrench: usePrevious

---
+++ app/hooks/usePrevious.ts
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from "react"
+
+const usePrevious = <T>(val: T) => {
+    const ref = useRef<T>();
+
+    useEffect(() => {
+        ref.current = val;
+    }, [val]);
+
+    return ref.current;
+};
+
+export default usePrevious;
